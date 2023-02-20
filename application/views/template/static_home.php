f8694253ba83330033d9a75b626b84d441a57ac2
RiskyAdit06 <risky.aditp06@gmail.com>
2023-02-20 21:33:26 +0700

    Up to Git myresume

---
+++ application/views/template/static_home.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <title>RiskyAdit - CV</title>
+  <meta name="generator" content="Jekyll v4.1.1" />
+  <meta property="og:title" content="RiskyAdit - CV" />
+  <meta name="author" content="Admin" />
+  <meta property="og:locale" content="en_US" />
+  <meta name="description" content="Jekyll Resume / CV / vCard template" />
+  <meta property="og:description" content="Jekyll Resume / CV / vCard template" />
+  <link rel="canonical" href="<?=base_url() ?>" />
+  <meta property="og:url" content="<?=base_url() ?>" />
+  <meta property="og:site_name" content="RiskyAdit - CV" />
+
+  <!-- Load Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link rel="stylesheet" href="<?=base_url() ?>assets/css/all.css">
+
+  <!-- Load CSS -->
+  <link rel="stylesheet" href="<?=base_url() ?>assets/css/style.css">
+  <link rel="stylesheet" href="<?=base_url() ?>assets/css/ionicons.css" />
+  <link rel="stylesheet" href="<?=base_url() ?>assets/css/magnific-popup.css" />
+  <link rel="stylesheet" href="<?=base_url() ?>assets/css/animate.css" />
+  <link rel="stylesheet" href="<?=base_url() ?>assets/css/owl.carousel.css" />
+  <link rel="stylesheet" href="<?=base_url() ?>assets/css/syntax.css" />
+  
+  <!-- Favicons -->
+  <link rel="shortcut icon" href="<?=base_url() ?>assets/images/favicons/favicon.ico"><link type="application/atom+xml" rel="alternate" href="feed.xml" title="RiskyAdit - CV" /></head><body class=" home">
+
+    <div class="page new-skin">
+
+      <!-- preloader -->
+      <div class="preloader">
+        <div class="centrize full-width">
+          <div class="vertical-center">
+            <div class="spinner">
+              <div class="double-bounce1"></div>
+              <div class="double-bounce2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- background -->
+      <div class="background gradient">
+        <ul class="bg-bubbles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+
+      <div class="s_overlay"></div>
+
+      <!-- Container -->
+      <div class="container opened" data-animation-in="fadeInLeft" data-animation-out="fadeOutLeft">
+
+        <!-- Header -->
+        <header class="header">
+          <!-- menu -->
+          <div class="top-menu">
+            <ul>
+
+              <li class="active">
+                <a href="<?=base_url() ?>#about-card">
+                  <span class="icon ion-person"></span>
+                  <span class="link">About</span>
+                </a>
+              </li>
+
+              <li>
+                <a href="<?=base_url() ?>#resume-card">
+                  <span class="icon ion-android-list"></span>
+                  <span class="link">Resume</span>
+                </a>
+              </li>
+
+              <li>
+                <a href="<?=base_url() ?>#works-card">
+                  <span class="icon ion-paintbrush"></span>
+                  <span class="link">portofolio</span>
+                </a>
+              </li>
+
+            </ul>
+          </div>
+
+        </header>
+
+        
+
+        <?=$contents; ?>
+
+
+      </div>
+    </div><!-- Plugins -->
+    <script src="<?=base_url() ?>assets/js/jquery.min.js"></script>
+    <script src="<?=base_url() ?>assets/js/jquery.validate.js"></script>
+    <script src="<?=base_url() ?>assets/js/jquery.magnific-popup.js"></script>
+    <script src="<?=base_url() ?>assets/js/imagesloaded.pkgd.js"></script>
+    <script src="<?=base_url() ?>assets/js/isotope.pkgd.js"></script>
+    <script src="<?=base_url() ?>assets/js/jquery.slimscroll.js"></script>
+    <script src="<?=base_url() ?>assets/js/owl.carousel.js"></script>
+    <script src="<?=base_url() ?>assets/js/typed.js"></script>
+    <script src="<?=base_url() ?>assets/js/rrssb.js"></script>
+    <script src="<?=base_url() ?>assets/js/simple-jekyll-search.js"></script>
+
+
+    <!-- Google map api -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDz2w7HUaWudHwd7AWQpCL48Qs050WOn9s"></script>
+
+
+    <!-- Main -->
+    <script src="<?=base_url() ?>assets/js/scripts.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125314689-11"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-125314689-11');
+    </script>
+
+    <div class="bsl-popup" data-theme="ryancv" data-category="jekyll">
+      <div class="bsl-popup__buttons"></div>
+      <div class="bsl-popup__content bsl-popup__content-related">
+        <div class="bsl-popup__menu"></div>
+        <div class="bsl-popup__tabs">
+          <div class="bsl-popup__tab bsl-popup__tab-demo"></div>
+          <div class="bsl-popup__tab bsl-popup__tab-all"></div>
+          <div class="bsl-popup__tab bsl-popup__tab-related"></div>
+          <div class="bsl-popup__tab bsl-popup__tab-version"></div>
+        </div>
+      </div>
+      <div class="bsl-popup__content bsl-popup__content-help"></div>
+    </div>
+    
+    <link rel="stylesheet" href="<?=base_url() ?>assets/css/style.css">
+    <script src="<?=base_url() ?>assets/js/scripts2.js"></script>
+    
+  </body>
+  </html>
